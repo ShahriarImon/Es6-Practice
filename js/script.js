d292f46c0590785363cc7d1a55934d0305ed8015
ShahriarImon <imon.shahriar012@gmail.com>
2022-06-13 18:37:58 +0600

    arraymap, arrayfilter, arrayfind

--- js/script.js
+++ js/script.js
@@ -39,8 +39,40 @@ const {road} = plane
 console.log(plane.getSum(plane.fuel,plane.road));
 console.log(plane);
 
-const fruits = ['banana', 'lichi', 'mango', 'guava'];
+const fruits = ['banana', 'jackFruit', 'lichi', 'mango', 'guava','apple'];
 const flowers = ['sapla', 'beli', 'joba', 'rose'];
-const all = [...fruits].concat( [...flowers]);
+const all = [...fruits,...flowers]
 const [f1, f2, ...fRest] = fruits;
 console.log(all);
+console.log(f1);
+console.log(typeof(f1));
+console.log(f2);
+console.log(fRest);
+console.log(fruits);
+
+
+const pakaFruits = fruits.map((element,index) => `${index+1} paka ${element}`);
+console.log(pakaFruits);
+
+const fiveLetterFruits = fruits.filter(element => element.length<=5)
+console.log(fiveLetterFruits);
+
+const actors = [
+    {name: 'Amir Khan', age : 60},
+    {name: 'Salman Khan', age : 49},
+    {name: 'Sharukh Khan', age : 48},
+    {name: 'NTR', age : 43}
+]
+
+const newArray = [];
+for(let i =0; i<actors.length; i++){
+    newArray.push(actors[i].name);
+}
+console.log(newArray);
+
+const ActorsName = actors.map((element, index) => `No ${index+1} - ${element.name} is ${element.age} years old`);
+console.log(ActorsName);
+
+
+console.log(actors.filter(element => element.age<50));
+console.log(actors.find(element => element.age<50));
